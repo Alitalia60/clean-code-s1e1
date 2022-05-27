13be8de1197df20030b993504c507b926b3aec12
Andrew Litvinov <lita.study@bk.ru>
2022-05-27 16:26:18 +0300

    feat: p.3.1 change selectors & id to one style

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById('new-task');//Add a new task.
 var addButton=document.getElementsByTagName('button')[0];//first button
-var incompleteTaskHolder=document.getElementById('incompleteTasks');//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById('incomplete-tasks');//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById('completed-tasks');//completed-tasks
 
 
@@ -25,7 +25,7 @@ var createNewTaskElement=function(taskString){
     var label=document.createElement('label');//label
     //input (text)
     var editInput=document.createElement('input');//text
-    //button.edit
+    //.button-action
     var editButton=document.createElement('button');//edit button
 
     //button.delete
@@ -131,7 +131,7 @@ var taskIncomplete=function(){
     console.log('Incomplete Task...');
 //Mark task as incomplete.
     //When the checkbox is unchecked
-    //Append the task list item to the #incompleteTasks.
+    //Append the task list item to the #incomplete-tasks.
     var listItem=this.parentNode;
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
@@ -156,8 +156,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log('bind list item events');
 //select ListItems children
     var checkBox=taskListItem.querySelector('input[type=checkbox]');
-    var editButton=taskListItem.querySelector('button.edit');
-    var deleteButton=taskListItem.querySelector('button.delete');
+    var editButton=taskListItem.querySelector('.button-action');
+    var deleteButton=taskListItem.querySelector('.button-delete');
 
 
     //Bind editTask to edit button.
